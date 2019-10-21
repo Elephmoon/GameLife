8096f2bb5b5385e0fce85b23d8f6a70410755d18
Alexandr Kizilow <alexandr.kizilow@gmail.com>
2019-10-21 17:52:03 +0300

    add function ReadNeighbors

--- main.cpp
+++ main.cpp
@@ -34,6 +34,26 @@ void WorldInit(Point world[][__WORLD_HEIGHT__])
     }
 }
 
+/*
+ * Read neighbors points.
+ */
+void ReadNeighbors(signed int nb[][2], unsigned int x, unsigned int y)
+{
+    unsigned int i, j;
+    unsigned int k = 0;
+
+    for (i = x - 1; i <= x + 1; i++) {
+        for (j = y - 1; j <= y + 1; j++) {
+            if (i == x && j == y) {
+                continue;
+            }
+            nb[k][0] = i;
+            nb[k][1] = j;
+            k++;
+        }
+    }
+}
+
 int main() {
     return 0;
 }
